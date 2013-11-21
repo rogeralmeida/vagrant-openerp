887176ddf1126b3f4cfb9cb137874d87daa3d8d5
Roger Almeida <roger.eduardo@gmail.com>
2013-11-21 15:21:29 -0200

    Removido o modulo rvm dos manifestos

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,15 +3,6 @@ class { 'locales':
   default_locale => 'en_US.UTF-8',
 }
 
-include rvm
-rvm::system_user { vagrant: ; }
-
-rvm_system_ruby {
-  'ruby-1.9.3-p448':
-  ensure => 'present',
-  require  => Exec['apt-get update'],
-  default_use => true;
-}
 
 class { 'postgresql::server':
   ip_mask_allow_all_users    => '0.0.0.0/0',
@@ -42,63 +33,9 @@ package { 'postgresql-server-dev-9.1':
     require  => Exec['apt-get update'],
 }
 
-$openerp_dependencies = ["python-dateutil","python-feedparser", "python-gdata", "python-ldap",
-"python-libxslt1", "python-lxml", "python-mako", "python-openid", "python-psycopg2", 
-"python-pybabel", "python-pychart", "python-pydot", "python-pyparsing", "python-reportlab", 
-"python-simplejson", "python-tz", "python-vatnumber", "python-vobject", "python-webdav",
-"python-werkzeug", "python-xlwt", "python-yaml", "python-zsi"]
-
-package{ $openerp_dependencies: 
-  ensure => installed,
-  require  => Exec['apt-get update'],
-}
-
-define download_file(
-        $site="",
-        $cwd="",
-        $creates="",
-        $require="",
-        $user="") {                                                                                         
-
-    exec { $name:                                                                                                                     
-        command => "/usr/bin/wget ${site}/${name}",                                                         
-        cwd => $cwd,
-        creates => "${cwd}/${name}",                                                              
-        require => $require,
-        user => $user,                                                                                                          
-    }
-
-}
-
-download_file { [                                                                                                                     
-      "openerp_7.0-20121227-075624-1_all.deb"
-    ]:                                                                                                                                
-    site => "http://nightly.openerp.com/7.0/nightly/deb/",
-    cwd => "${repo_root}/",                                                                            
-    creates => "${repo_root}/$name",                                                                  
-    require => Package["wget"],                                                                  
-    user => $repo_owner,                                                                                                              
-}
-
-package { 'openerp' :
-  ensure => installed,
-  source => '/tmp/openerp_7.0-20121227-075624-1_all.deb',
-  provider => 'dpkg',
-}
-
 package { 'wget':
   ensure => installed,
   require  => Exec['apt-get update'],
-}
-
-exec { 'enable ufw':
-  command => 'sudo ufw enable',
-  path => ['/usr/bin/','usr/local/bin/', '/bin/'],
-}
-
-exec { 'allow connections on port 8069':
-  command => 'sudo ufw allow 8069',
-  path => ['/usr/bin/','usr/local/bin/', '/bin/'],
 }
 
 exec { "apt-get update":
